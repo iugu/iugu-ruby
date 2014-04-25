91248c39b1cc6bbe405d32e73e556ec4fda1b33c
Alexandre Paez <alepaezseq@gmail.com>
2014-04-25 11:18:22 -0300

    [ ! ] Plan Fetch by identifier

--- lib/iugu/plan.rb
+++ lib/iugu/plan.rb
@@ -4,5 +4,10 @@ module Iugu
     include Iugu::APICreate
     include Iugu::APISave
     include Iugu::APIDelete
+
+    def self.fetch_by_identifier(identifier)
+      Iugu::Factory.create_from_response(object_type, APIRequest.request("GET", "#{url}/identifier/#{identifier}"))
+    end
   end
+
 end
