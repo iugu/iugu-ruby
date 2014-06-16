b8bfc00ce9f5274fe62370425c3108689ed325d7
Marcelo Paez Sequeira <nkr@Capeta.local>
2014-06-16 14:49:21 -0300

    [ ! ] Added success method on charge

--- lib/iugu/charge.rb
+++ lib/iugu/charge.rb
@@ -2,6 +2,10 @@ module Iugu
   class Charge < APIResource
     include Iugu::APICreate
 
+    def success
+      @attributes["success"] || false
+    end
+    
     def invoice
       return false unless @attributes['invoice_id']
       Invoice.fetch @attributes['invoice_id']
