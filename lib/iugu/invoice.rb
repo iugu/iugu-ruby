36682e9c90e67e232ee1a50ca831b5f78e2875ba
Lucas <lucas.oliveira@iugu.com>
2017-11-30 18:33:06 -0200

    Change Iugu::Invoice#duplicate to accept more params

--- lib/iugu/invoice.rb
+++ lib/iugu/invoice.rb
@@ -6,12 +6,14 @@ module Iugu
     include Iugu::APIDelete
 
     def customer
-      return false unless @attributes["customer_id"]
-      Customer.fetch @attributes["customer_id"]
+      return false unless @attributes['customer_id']
+      Customer.fetch @attributes['customer_id']
     end
 
     def cancel
-      copy Iugu::Factory.create_from_response(self.class.object_type, APIRequest.request("PUT", "#{self.class.url(self.id)}/cancel"))
+      copy Iugu::Factory.create_from_response(self.class.object_type,
+                                              APIRequest.request('PUT',
+                                                                 "#{self.class.url(self.id)}/cancel"))
       self.errors = nil
       true
     rescue Iugu::RequestWithErrors => ex
@@ -20,7 +22,9 @@ module Iugu
     end
 
     def refund
-      copy Iugu::Factory.create_from_response(self.class.object_type, APIRequest.request("POST", "#{self.class.url(self.id)}/refund"))
+      copy Iugu::Factory.create_from_response(self.class.object_type,
+                                              APIRequest.request('POST',
+                                                                 "#{self.class.url(self.id)}/refund"))
       self.errors = nil
       true
     rescue Iugu::RequestWithErrors => ex
@@ -28,8 +32,11 @@ module Iugu
       false
     end
 
-    def duplicate(due_date)
-      copy Iugu::Factory.create_from_response(self.class.object_type, APIRequest.request("POST", "#{self.class.url(self.id)}/duplicate", { due_date: due_date, ignore_canceled_email: true }))
+    def duplicate(attributes = {})
+      copy Iugu::Factory.create_from_response(self.class.object_type,
+                                              APIRequest.request('POST',
+                                                                 "#{self.class.url(self.id)}/duplicate",
+                                                                 attributes ))
       self.errors = nil
       true
     rescue Iugu::RequestWithErrors => ex
--- lib/iugu/payment_method.rb
+++ lib/iugu/payment_method.rb
@@ -5,10 +5,10 @@ module Iugu
     include Iugu::APISave
     include Iugu::APIDelete
 
-    def self.url(options = {}) 
-      customer_id = options[:customer_id] || options["customer_id"]
-      id = options[:id] || options["id"]
-      raise StandardError, "Missing Customer ID" unless customer_id
+    def self.url(options = {})
+      customer_id = options[:customer_id] || options['customer_id']
+      id = options[:id] || options['id']
+      raise StandardError, 'Missing Customer ID' unless customer_id
       "#{Customer.url customer_id}/#{self.object_base_uri}" + self.relative_url(id)
     end
   end
