22d3c3fb2822716581ed0f98fdf9e9ac72ce00cc
Lucas <lucas.oliveira@iugu.com>
2017-11-30 14:19:31 -0200

    Add specs to Iugu::Customer.create

--- lib/iugu/api_create.rb
+++ lib/iugu/api_create.rb
@@ -2,7 +2,10 @@ module Iugu
   module APICreate
     module ClassMethods
       def create(attributes = {})
-        Iugu::Factory.create_from_response self.object_type,  APIRequest.request("POST", self.url(attributes), attributes)
+        Iugu::Factory.create_from_response(self.object_type,
+                                           APIRequest.request('POST',
+                                                              self.url(attributes),
+                                                              attributes))
       rescue Iugu::RequestWithErrors => ex
         obj = self.new
         obj.set_attributes attributes, true
