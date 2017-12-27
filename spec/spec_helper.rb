3c7a1367988df4dacf22213a6bc2ac0f29c51a0f
Lucas <just.lucasoliveira@gmail.com>
2017-12-27 13:55:55 -0200

    Change VCR to allow request without recording cassettes
    This is needed in order to write new specs without having to delete
    cassettes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,9 @@ VCR.configure do |config|
   config.cassette_library_dir = 'spec/cassettes'
   config.hook_into :webmock
   config.configure_rspec_metadata!
+
+  # needed to write new specs without having to delete cassettes
+  config.allow_http_connections_when_no_cassette = true
 end
 
 RSpec.configure do |config|
