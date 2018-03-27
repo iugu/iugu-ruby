dfb2bdcc546be436a4477fe435ebdeae337b37e7
Nelson Haraguchi <nelsonmhjr@gmail.com>
2018-03-27 15:18:54 -0300

    Fix, was missing data hash

--- lib/iugu/api_fetch.rb
+++ lib/iugu/api_fetch.rb
@@ -2,7 +2,7 @@ module Iugu
   module APIFetch
     def refresh(token = nil)
       copy Iugu::Factory.create_from_response(
-        self.class.object_type, APIRequest.request('GET', self.class.url(self.id), token)
+        self.class.object_type, APIRequest.request('GET', self.class.url(self.id), {}, token)
       )
       self.errors = nil
       true
@@ -14,7 +14,7 @@ module Iugu
     module ClassMethods
       def fetch(options = nil, token = nil)
         Iugu::Factory.create_from_response(
-          self.object_type, APIRequest.request('GET', self.url(options), token)
+          self.object_type, APIRequest.request('GET', self.url(options), {}, token)
         )
       end
     end
