10f08b248d8e15f264eca0979c3c73c015916619
Nelson Haraguchi <nelsonmhjr@gmail.com>
2018-03-27 13:18:09 -0300

    Ability to fetch and refresh using other account

--- lib/iugu/api_fetch.rb
+++ lib/iugu/api_fetch.rb
@@ -1,9 +1,9 @@
 module Iugu
   module APIFetch
-    def refresh
-      copy Iugu::Factory.create_from_response(self.class.object_type,
-                                              APIRequest.request('GET',
-                                                                 self.class.url(self.id)))
+    def refresh(token = nil)
+      copy Iugu::Factory.create_from_response(
+        self.class.object_type, APIRequest.request('GET', self.class.url(self.id), token)
+      )
       self.errors = nil
       true
     rescue Iugu::RequestWithErrors => ex
@@ -12,10 +12,10 @@ module Iugu
     end
 
     module ClassMethods
-      def fetch(options = nil)
-        Iugu::Factory.create_from_response(self.object_type,
-                                           APIRequest.request('GET',
-                                                              self.url(options)))
+      def fetch(options = nil, token = nil)
+        Iugu::Factory.create_from_response(
+          self.object_type, APIRequest.request('GET', self.url(options), token)
+        )
       end
     end
 
